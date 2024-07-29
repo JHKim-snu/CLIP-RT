1d93ee5eb7b2d2d34d9bcb9d235eb3e042715046
Junghyun Kim <80503012+JHKim-snu@users.noreply.github.com>
2024-07-29 21:26:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,17 @@ import base64
 import socket
 import numpy as np
 import time
+from peft import PeftModel 
+
+import socket
+import base64
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--mode", dest="mode", action="store", default="full")
+args = parser.parse_args()
 
 # ROBOT
 # import urx
@@ -20,28 +31,113 @@ import time
 # image = cv_img
 
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+vla_path = '/home/jhkim/data/clipRT/openvla/vla-scripts/runs/openvla-7b+clip_rt_example+b8+lr-0.0005+lora-r32+dropout-0.0'
+adapter_path = "/home/jhkim/data/clipRT/openvla/vla-scripts/adapter-tmp/openvla-7b+clip_rt_example+b8+lr-0.0005+lora-r32+dropout-0.0"
 
 # Load Processor & VLA
-processor = AutoProcessor.from_pretrained("openvla/openvla-7b", trust_remote_code=True)
+# processor = AutoProcessor.from_pretrained("openvla/openvla-7b", trust_remote_code=True)
+# vla = AutoModelForVision2Seq.from_pretrained(
+#     "openvla/openvla-7b",
+#     attn_implementation="flash_attention_2",  # [Optional] Requires `flash_attn`
+#     torch_dtype=torch.bfloat16,
+#     low_cpu_mem_usage=True,
+#     trust_remote_code=True,
+# ).to("cuda")
+
+processor = AutoProcessor.from_pretrained(vla_path, trust_remote_code=True)
 vla = AutoModelForVision2Seq.from_pretrained(
-    "openvla/openvla-7b",
+    vla_path,
     attn_implementation="flash_attention_2",  # [Optional] Requires `flash_attn`
     torch_dtype=torch.bfloat16,
     low_cpu_mem_usage=True,
     trust_remote_code=True,
 ).to("cuda")
 
-image = Image.open("../RT/azure_test.png")
 
-instruction = "pick the cup"
-prompt = "In: What action should the robot take to {}?\nOut:".format(instruction)
+# vla = PeftModel.from_pretrained(vla, adapter_name=adapter_path)
+# vla = AutoModelForVision2Seq.from_pretrained(adapter_path)
 
-# Predict Action (7-DoF; un-normalize for BridgeData V2)
-inputs = processor(prompt, image).to("cuda:0", dtype=torch.bfloat16)
-action = vla.predict_action(**inputs, unnorm_key="bridge_orig", do_sample=False)
+# vla.load_adapter(adapter_path)
 
-print(action)
-# Execute...
-# robot.act(action, ...)
+# vla.eval()
+
+
+if args.mode == "serveronly":
+
+    image = Image.open("/home/jhkim/data/clipRT/rlds_dataset_builder/clip_rt_example/data/pick_test/pick_test_0/0.jpg")
+
+    instruction = "pick up the blue bottle"
+    prompt = "In: What action should the robot take to {}?\nOut:".format(instruction)
+
+    # Predict Action (7-DoF; un-normalize for BridgeData V2)
+    inputs = processor(prompt, image).to("cuda:0", dtype=torch.bfloat16)
+    action = vla.predict_action(**inputs, unnorm_key="clip_rt", do_sample=False)
+
+    print(action)
+
+
+elif args.mode == "full":
+    # Execute...
+    # robot.act(action, ...)
+
+    image_root_path = "./raw_data/images/test/"
+    HOST = '114.110.129.13'
+    PORT = 9998
+
+    srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv_sock.bind((HOST, PORT))
+    srv_sock.listen()
+    cli_sock, addr = srv_sock.accept()
+    print(f'Connected by: {addr}')
+
+    image_cnt = 0
+    while True:
+        try:
+            
+            instruction = input("Instruction: ")
+            # Receive CV2 Image
+            length = int(cli_sock.recv(65).decode('utf-8'), 2)
+            buf = b''
+            while length:
+                newbuf = cli_sock.recv(length)
+                buf += newbuf
+                length -= len(newbuf)
+            print("image recieved from the robot!")
+            
+            data = np.frombuffer(base64.b64decode(buf), np.uint8)
+            cv2_img = cv2.imdecode(data, cv2.IMREAD_COLOR)
+            # save received image
+            dt = datetime.now()
+            save_image_path = image_root_path + '{}_{}_{}_{}_{}_{}.png'.format(dt.year,dt.month,dt.day,dt.hour,dt.minute,dt.second)
+            cv2.imwrite(save_image_path, cv2_img)
+            image_cnt += 1
+            
+            image = Image.open(save_image_path)
+                        
+            prompt = "In: What action should the robot take to {}?\nOut:".format(instruction)
+            
+            inputs = processor(prompt, image).to("cuda:0", dtype=torch.bfloat16)
+            action = vla.predict_action(**inputs, unnorm_key="clip_rt", do_sample=False)
+            
+            
+            action_info = f'{action[0]};{action[1]};{action[2]};{action[3]};{action[4]};{action[5]};{action[6]}'
+            print(f'Send {action_info}')
+            print('\n')
+            cli_sock.send(action_info.encode())
+
+            ###############AAAAAAAAAAAAAAAAAA################
+            #############################
+
+
+        except KeyboardInterrupt:
+            print('\n Server Ctrl-c')
+            break
+        except ValueError:
+            print('\n Client Closed')
+            break
+
+    cli_sock.close()
+    srv_sock.close()
